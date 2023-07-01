2e70d547027dab7e30a37d9de97ddb4b0b76f808
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-02 03:07:22 +0600

    PostDto

--- src/post/post.dto.ts
+++ src/post/post.dto.ts
@@ -1,1 +1,12 @@
-export class PostDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+
+export class PostDto {
+  @ApiProperty({ description: 'Please enter your title' })
+  @IsString()
+  title: string;
+
+  @ApiPropertyOptional({ description: 'Please enter your valid description' })
+  @IsEmail()
+  description?: string;
+}
