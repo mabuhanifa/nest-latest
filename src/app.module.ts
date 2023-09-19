eb3fd1940cc0261917fed875a65b2660dc450671
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-19 16:56:01 +0600

    resources created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -6,6 +7,7 @@ import { PostModule } from './post/post.module';
 import { IamModule } from './iam/iam.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
   imports: [
@@ -16,7 +18,9 @@ import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql'],
+      driver: ApolloDriver,
     }),
+    DonationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
