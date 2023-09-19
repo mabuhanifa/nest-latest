786f191ff80f0041d0dd2d2376a5732a3e3347a1
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-19 17:06:57 +0600

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
 
   app.useGlobalPipes(new ValidationPipe());
 
