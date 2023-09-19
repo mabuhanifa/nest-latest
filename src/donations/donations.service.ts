e11ad696447d103d41bee3c56b3af140ddc84529
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-19 18:42:54 +0600

    PrismaService included

--- src/donations/donations.service.ts
+++ src/donations/donations.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDonationInput } from './dto/create-donation.input';
 import { UpdateDonationInput } from './dto/update-donation.input';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 const data = [
   { id: 1, amount: 100 },
@@ -9,6 +10,8 @@ const data = [
 
 @Injectable()
 export class DonationsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createDonationInput: CreateDonationInput) {
     data.push(createDonationInput);
     return createDonationInput;
