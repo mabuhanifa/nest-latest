a1162126216c61b58558bb31166bb5b503bdadb9
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-19 17:48:27 +0600

    graphql.ts

---
+++ src/graphql.ts
@@ -0,0 +1,40 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export class CreateDonationInput {
+    id?: Nullable<number>;
+    amount?: Nullable<number>;
+}
+
+export class UpdateDonationInput {
+    id?: Nullable<number>;
+    amount?: Nullable<number>;
+}
+
+export class Donation {
+    id?: Nullable<number>;
+    amount?: Nullable<number>;
+}
+
+export abstract class IQuery {
+    abstract donations(): Nullable<Donation>[] | Promise<Nullable<Donation>[]>;
+
+    abstract donation(id: number): Nullable<Donation> | Promise<Nullable<Donation>>;
+}
+
+export abstract class IMutation {
+    abstract createDonation(createDonationInput: CreateDonationInput): Donation | Promise<Donation>;
+
+    abstract updateDonation(updateDonationInput: UpdateDonationInput): Donation | Promise<Donation>;
+
+    abstract removeDonation(id: number): Nullable<Donation> | Promise<Nullable<Donation>>;
+}
+
+type Nullable<T> = T | null;
